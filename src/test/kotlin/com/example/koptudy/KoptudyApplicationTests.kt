5bc450ca4d4b59ec2ec9f78d53109e5141954aa4
lyutvs <lyutvs@gmail.com>
2022-04-13 12:31:40 +0900

    ⚙️ :: 프로젝트 생성

---
+++ src/test/kotlin/com/example/koptudy/KoptudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.koptudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KoptudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
