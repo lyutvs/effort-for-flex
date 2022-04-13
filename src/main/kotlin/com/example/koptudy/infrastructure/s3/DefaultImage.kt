6d8c00cb328f4734e7f11525be64a2b8c515fc2c
lyutvs <lyutvs@gmail.com>
2022-04-13 20:06:40 +0900

    📑 :: DefaultImage

---
+++ src/main/kotlin/com/example/koptudy/infrastructure/s3/DefaultImage.kt
@@ -0,0 +1,13 @@
+package com.example.koptudy.infrastructure.s3
+
+import lombok.AccessLevel
+import lombok.NoArgsConstructor
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+class DefaultImage {
+
+    companion object {
+        const val USER_PROFILE_IMAGE = "'a'"
+    }
+
+}
