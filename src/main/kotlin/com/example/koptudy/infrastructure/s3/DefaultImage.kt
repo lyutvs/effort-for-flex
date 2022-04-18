843baf6f5e4d43884fa7acb655a814bd9564635f
lyutvs <lyutvs@gmail.com>
2022-04-18 12:52:36 +0900

    ⚡️ :: DefaultImage 추가

--- src/main/kotlin/com/example/koptudy/infrastructure/s3/DefaultImage.kt
+++ src/main/kotlin/com/example/koptudy/infrastructure/s3/DefaultImage.kt
@@ -6,8 +6,10 @@ import lombok.NoArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 class DefaultImage {
 
+
     companion object {
         const val USER_PROFILE_IMAGE = "'a'"
+        const val COMPANY_PROFILE_IMAGE = "'a'"
     }
 
 }
