790c9be4d23bf07e3cb2f180ad4e0fc131a1a664
lyutvs <lyutvs@gmail.com>
2022-04-18 13:01:46 +0900

    ⚡️ :: holiday 연관관계 매핑

--- src/main/kotlin/com/example/koptudy/domain/holiday/HolidayType.kt
+++ src/main/kotlin/com/example/koptudy/domain/holiday/HolidayType.kt
@@ -1,11 +1,19 @@
 package com.example.koptudy.domain.holiday
 
+import com.example.koptudy.domain.holiday.domain.Holiday
 import org.hibernate.validator.constraints.Length
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.validation.constraints.NotNull
 
 class HolidayType(
 
-    title: String
+    title: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "holiday_id")
+    var holiday: Holiday
 
 ) {
 
