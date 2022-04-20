261176725e97c49df4e0b2390ac55674139529e3
lyutvs <lyutvs@gmail.com>
2022-04-20 13:27:12 +0900

    ♻️ :: 임포트 정리

--- src/main/kotlin/com/example/koptudy/KoptudyApplication.kt
+++ src/main/kotlin/com/example/koptudy/KoptudyApplication.kt
@@ -2,7 +2,6 @@ package com.example.koptudy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 class KoptudyApplication
