1f8c8a938ef5abd7efd094444653414ee9ac4ad4
lyutvs <lyutvs@gmail.com>
2022-04-20 14:53:36 +0900

    ⚡️ :  @Embeddable

--- src/main/kotlin/com/example/koptudy/domain/holiday/domain/HolidayListId.kt
+++ src/main/kotlin/com/example/koptudy/domain/holiday/domain/HolidayListId.kt
@@ -6,12 +6,10 @@ import javax.persistence.*
 @Embeddable
 class HolidayListId(
 
-    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "holiday_id")
     var holiday: Holiday,
 
-    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "holiday_type_id")
     var holidayType: HolidayType
