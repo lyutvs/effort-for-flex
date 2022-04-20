f064a0abd72476ecc81f7f4b5f71ddc4e4b2494d
lyutvs <lyutvs@gmail.com>
2022-04-20 13:19:12 +0900

    ⚡️ :: HolidayType

--- src/main/kotlin/com/example/koptudy/domain/holiday/domain/HolidayType.kt
+++ src/main/kotlin/com/example/koptudy/domain/holiday/domain/HolidayType.kt
@@ -1,20 +1,22 @@
 package com.example.koptudy.domain.holiday.domain
 
+import com.example.koptudy.domain.company.domain.Company
+import com.example.koptudy.global.entity.BaseTimeIdEntity
 import org.hibernate.validator.constraints.Length
-import javax.persistence.FetchType
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
+import javax.persistence.*
 import javax.validation.constraints.NotNull
 
+@Table(name = "`holiday_type`")
+@Entity
 class HolidayType(
 
     title: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "holiday_id")
-    var holiday: Holiday
+    @JoinColumn(name = "company_id")
+    var company: Company
 
-) {
+): BaseTimeIdEntity() {
 
     @NotNull
     @Length(max = 20)
